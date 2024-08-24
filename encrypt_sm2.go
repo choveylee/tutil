7780c0e95cf29bfd4bd7ff7551aefb4fd34c3e92
lidonglin@rcrai.com <lidonglin@rcrai.com>
2024-08-24 13:58:36 +0800

    Update encrypt_sm2.go

--- encrypt_sm2.go
+++ encrypt_sm2.go
@@ -9,31 +9,19 @@
 package tutil
 
 import (
-	"context"
 	"crypto/rand"
-	"encoding/hex"
-	"fmt"
 
 	"github.com/tjfoc/gmsm/sm2"
 	"github.com/tjfoc/gmsm/x509"
 )
 
-func Sm2Decrypt(cipherText string, hexPrivateKey string) ([]byte, error) {
+func Sm2Decrypt(cipherText []byte, hexPrivateKey string) ([]byte, error) {
 	privateKey, err := x509.ReadPrivateKeyFromHex(hexPrivateKey)
 	if err != nil {
 		return nil, err
 	}
 
-	cipher2Text, err := hex.DecodeString("04" + cipherText)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(cipher2Text) < 33 {
-		return nil, fmt.Errorf("cipher lens")
-	}
-
-	plainText, err := sm2.Decrypt(privateKey, cipher2Text, sm2.C1C3C2)
+	plainText, err := sm2.Decrypt(privateKey, cipherText, sm2.C1C3C2)
 	if err != nil {
 		return nil, err
 	}
@@ -41,7 +29,7 @@ func Sm2Decrypt(cipherText string, hexPrivateKey string) ([]byte, error) {
 	return plainText, nil
 }
 
-func Sm2Encrypt(ctx context.Context, plainText []byte, hexPublicKey string) ([]byte, error) {
+func Sm2Encrypt(plainText []byte, hexPublicKey string) ([]byte, error) {
 	publicKey, err := x509.ReadPublicKeyFromHex(hexPublicKey)
 	if err != nil {
 		return nil, err
@@ -55,7 +43,7 @@ func Sm2Encrypt(ctx context.Context, plainText []byte, hexPublicKey string) ([]b
 	return cipherText, nil
 }
 
-func GenSm2KeyPair(ctx context.Context) (string, string, error) {
+func GenSm2KeyPair() (string, string, error) {
 	privateKey, err := sm2.GenerateKey(rand.Reader)
 	if err != nil {
 		return "", "", err
